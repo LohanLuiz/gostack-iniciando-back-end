1550f307402d4957845348f8c10014d23bce5a7c
Lohan Luiz <lohanluiz12@gmail.com>
2020-12-17 10:58:13 -0400

    progress: update avatar

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -1,4 +1,8 @@
 import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +13,21 @@ interface Request {
 class UpdateUserAvatarService {
   public async execute({ user_id, avatarFilename }: Request): Promise<void> {
     const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
   }
 }
 
