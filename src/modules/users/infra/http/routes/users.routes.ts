22c8d61946797ed7c6413b08ea22fd3177dcce97
Lohan Luiz <lohanluiz12@gmail.com>
2021-01-18 23:35:46 -0400

    finished the structure DDD part

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import { container } from 'tsyringe';
 
 import UsersController from '@modules/users/infra/http/controllers/UsersController';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', usersController.create);
@@ -18,25 +18,7 @@ usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    const userWithoutPassword = {
-      id: user.id,
-      avatarFilename: request.file.filename,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json(userWithoutPassword);
-  },
+  userAvatarController.update,
 );
 
 export default usersRouter;
