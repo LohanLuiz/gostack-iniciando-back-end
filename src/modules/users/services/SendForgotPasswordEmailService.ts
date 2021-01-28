52c8839e818e01f362639f6da4d058b6e6d6f06f
Lohan Luiz <lohanluiz12@gmail.com>
2021-01-28 16:52:46 -0400

    progress: added more test to check if email exists

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 // import User from '../infra/typeorm/entities/User';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
@@ -21,9 +21,15 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExists) {
+      throw new AppError('User does not exists');
+    }
+
     await this.mailProvider.sendMail(
       email,
-      'Pedido de recuperação de senha recebido',
+      'Pedido de recuperação de senha recebido.',
     );
   }
 }
