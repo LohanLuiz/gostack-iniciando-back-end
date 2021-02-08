7c0eb2db6f71c39d5fe82eda1cb14161b5d52631
Lohan Luiz <lohanluiz12@gmail.com>
2021-02-08 19:45:33 -0400

    adding celebrate with the routes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,9 @@
+import 'reflect-metadata';
+
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
-import 'reflect-metadata';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -17,20 +19,20 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
+app.use(errors());
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
   }
 
   console.error(err);
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
 });
 
 app.listen(3333, () => {
